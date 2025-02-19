0d5e14310c62dccef077eb6aae137b7b1ca5eab3
Bert <98714764+BertNK@users.noreply.github.com>
2025-02-19 11:58:32 +0100

    upd (first tests and trying out stuffs)

--- vite/src/main.jsx
+++ vite/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Home from './home.jsx'
+import './css/Global.css'
+import './css/Main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Home />
+  </StrictMode>
 )
